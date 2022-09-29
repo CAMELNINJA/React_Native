f167c7c1676bba4f8056f51ce164468866edbbca
Camel Ninla <rfvbkm0220@gmail.com>
2022-09-29 23:39:39 +0300

    feat : firs app

--- App.tsx
+++ App.tsx
@@ -1,10 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Image, ScrollView, StyleSheet, Text, TextInput, View} from 'react-native';
+import {useState} from "react";
+
+const image = require('./finder.png');
 
 export default function App() {
-  return (
+  const [name, setName] = useState("200");
+  var stat= 'https://http.cat/'+name
+   var numbers = ['ToDo', "Doing", 'Done', 'asfdas','afdsfad'];
+   var viewList=numbers.map(function (num, i){return (
+       <View><Image source={image} style={styles.imgHor}/><Text style={styles.textHor}>{num}</Text></View>)
+   })
+    return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={{marginTop:100}}><Text style={styles.text}>hi its first app CAMELNINGA</Text>
+      <Image
+          style={styles.tinyLogo}
+          source={ {uri: stat,}}
+          ></Image>
+      </View>
+      <View style={styles.container}>
+        <Text > Status Code</Text>
+        <TextInput style={styles.input} placeholder="200" maxLength={3}  onChangeText={(value) => setName(value)}/>
+      </View>
+      <ScrollView style={styles.scrollView} horizontal={true}>
+        <View style={styles.big} >
+          {viewList}
+          </View>
+
+
+        </ScrollView>
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <Text style={{fontSize:16}}>Раз <Text style={{fontWeight: 'bold'}}>два три </Text> четыре пять </Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +44,40 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text:{
+    fontSize:16,
+    fontWeight: 'bold'
+  },
+  tinyLogo: {
+    width: 300,
+    height: 300,
+    margin:10,
+  },
+  big:{
+    width: 60,
+    marginLeft: 60,
+    flexDirection:"row"
+  },
+  scrollView:{
+    width: 500,
+    height:100
+  },
+  textHor:{
+    marginRight: 10,
+    marginLeft:4,
+  },
+  imgHor:{
+    marginLeft:10,
+    marginRight:4,
+    width: 20,
+    height: 20
+  },
+  input: {
+    borderColor: "gray",
+    width: "100%",
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
   },
 });
