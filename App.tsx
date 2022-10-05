0bce4bd480cf5d96a439d6e2df74ec9a58cded53
Camel Ninla <rfvbkm0220@gmail.com>
2022-10-06 00:55:20 +0300

    feat : front calling

--- App.tsx
+++ App.tsx
@@ -2,38 +2,41 @@ import { StatusBar } from 'expo-status-bar';
 import {Image, ScrollView, StyleSheet, Text, TextInput, View} from 'react-native';
 import {useState} from "react";
 
-const image = require('./finder.png');
-
+const icons = {
+    image:require('./finder.png'),
+    icon:require('./628293_avatar_female_headset_person_support_icon.png'),
+    video:require('./6579051_and_camera_computers_hardware_video_icon.png'),
+    call:require('./9055406_bxs_phone_icon.png'),
+    sound:require('./2203528_lound_sound_speaker_volume_icon.png'),
+    mute: require('./6351966_microphone_mute_record_song_sound_icon.png')
+}
 export default function App() {
   const [name, setName] = useState("200");
   var stat= 'https://http.cat/'+name
    var numbers = ['ToDo', "Doing", 'Done', 'asfdas','afdsfad'];
    var viewList=numbers.map(function (num, i){return (
-       <View><Image source={image} style={styles.imgHor}/><Text style={styles.textHor}>{num}</Text></View>)
+       <View>
+
+       </View>)
    })
     return (
     <View style={styles.container}>
-      <View style={{marginTop:100}}><Text style={styles.text}>hi its first app CAMELNINGA</Text>
-      <Image
-          style={styles.tinyLogo}
-          source={ {uri: stat,}}
-          ></Image>
-      </View>
-      <View style={styles.container}>
-        <Text > Status Code</Text>
-        <TextInput style={styles.input} placeholder="200" maxLength={3}  onChangeText={(value) => setName(value)}/>
-      </View>
-      <ScrollView style={styles.scrollView} horizontal={true}>
-        <View style={styles.big} >
-          {viewList}
-          </View>
-
-
-        </ScrollView>
-      <View style={{ flex: 1, justifyContent: 'center' }}>
-        <Text style={{fontSize:16}}>Раз <Text style={{fontWeight: 'bold'}}>два три </Text> четыре пять </Text>
-      </View>
-      <StatusBar style="auto" />
+        <View style={{margin:10}}>
+        <View style={styles.buttonStart}>
+            <Image style={styles.imgVideo} source={icons.video}></Image>
+            <Text style={styles.text}>WHATSAPP CALL</Text>
+        </View>
+        <View ><Text style={styles.textBig} >Marc Johnson</Text></View>
+        <View ><Text style={styles.text}>CALLING</Text></View>
+        </View>
+        <View style={{alignItems:"baseline"}}><Image style={styles.imgBig}  source={icons.icon}></Image></View>
+        <View style={styles.center}>
+        <View style={styles.buttons}>
+            <Image style={styles.imgSmal} source={icons.sound}></Image>
+            <Image style={styles.imgSmal} source={icons.call}></Image>
+            <Image style={styles.imgSmal} source={icons.mute}></Image>
+        </View>
+        </View>
     </View>
   );
 }
@@ -41,43 +44,43 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#276056',
   },
-  text:{
-    fontSize:16,
-    fontWeight: 'bold'
-  },
-  tinyLogo: {
-    width: 300,
-    height: 300,
-    margin:10,
-  },
-  big:{
-    width: 60,
-    marginLeft: 60,
-    flexDirection:"row"
-  },
-  scrollView:{
-    width: 500,
-    height:100
-  },
-  textHor:{
-    marginRight: 10,
-    marginLeft:4,
-  },
-  imgHor:{
-    marginLeft:10,
-    marginRight:4,
-    width: 20,
-    height: 20
-  },
-  input: {
-    borderColor: "gray",
-    width: "100%",
-    borderWidth: 1,
-    borderRadius: 10,
-    padding: 10,
-  },
+    text:{
+        fontSize:10,
+      color:`#FFF`
+    },
+    textBig:{
+        fontSize:30,
+        color:`#FFF`
+    },
+    buttons:{
+      flexDirection:"row",
+        justifyContent: "space-around",
+
+    },
+    center:{
+      flex:1,
+        justifyContent:"center",
+       alignContent:"center",
+
+    },
+    buttonStart:{
+        flexDirection:"row",
+        justifyContent: "flex-start",
+        marginBottom:5,
+    },
+    imgSmal:{
+      height:25,
+        width:25,
+    },
+    imgVideo:{
+        height:25,
+        width:25,
+        marginRight:45,
+    },
+    imgBig:{
+     height:300,
+      width:"100%",
+    },
 });
