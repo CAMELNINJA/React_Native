1c795b699c759582200febe1cdca445226b554a4
Camel Ninla <rfvbkm0220@gmail.com>
2022-12-11 19:06:28 +0300

    feat : mob x clicker

--- App.tsx
+++ App.tsx
@@ -1,77 +1,26 @@
-
-
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View,Button} from 'react-native';
 import {observer} from 'mobx-react-lite'
 import {makeAutoObservable} from "mobx";
 
-class Counter {
-    count = 0
-
-    private FONT_SIZE_INITIAL = 10
+class Clicker {
+    clicks = 0
 
     constructor() {
         makeAutoObservable(this)
     }
 
-    onPress(isAddition: Boolean) {
-        let beAdded = isAddition ? 1 : -1
-        this.count += beAdded
-    }
-
-    fontSize() {
-        return Math.floor(Math.abs(this.count) / 5) + this.FONT_SIZE_INITIAL
+    addClick() {
+        this.clicks += 1
     }
 }
 
-const counter = new Counter()
+const clicker = new Clicker()
 
 const App = observer(() => (
-    <View style={styles.container}>
-        <View style={styles.countContainer}>
-            <Text style={{fontSize: counter.fontSize()}}>Count: {counter.count}</Text>
-        </View>
-        <View style={styles.countersContainer}>
-            <TouchableOpacity
-                style={styles.button}
-                onPress={() => {
-                    counter.onPress(true)
-                }}
-            >
-                <Text>+</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity
-                style={styles.button}
-                onPress={() => {
-                    counter.onPress(false)
-                }}
-            >
-                <Text>-</Text>
-            </TouchableOpacity>
-        </View>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>{clicker.clicks}</Text>
+        <Button onPress={() => clicker.addClick()} title="click" />
     </View>
 ))
 
-export default App
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        paddingHorizontal: 16
-    },
-    button: {
-        alignItems: "center",
-        backgroundColor: "#DDDDDD",
-        padding: 16
-    },
-    countContainer: {
-        alignItems: "center",
-        padding: 16,
-    },
-    countersContainer: {
-        flexDirection: "row",
-        justifyContent: "space-evenly",
-        padding: 16
-    }
-});
+export default App;
